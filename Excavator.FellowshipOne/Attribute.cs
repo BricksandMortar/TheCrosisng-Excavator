c83ce520d991dfeecd608582de9e9220d7483f73
Arran France <arranf@gmail.com>
2016-09-23 11:38:42 +0100

    Add attributes

--- Excavator.FellowshipOne/Attribute.cs
+++ Excavator.FellowshipOne/Attribute.cs
@@ -21,8 +21,16 @@ namespace Excavator.F1
             var personAttributes = attributeService.GetByEntityTypeId( PersonEntityTypeId ).ToList();
 
             var childSponsorshipLocationAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "ChildSponsorshipLocation", StringComparison.InvariantCultureIgnoreCase ) ) );
-            var facebookAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "Facebook", StringComparison.InvariantCultureIgnoreCase ) ) );
-            var instagramAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "Instagram", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var childSponsorshipStartDateAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "ChildSponsorshipStartDate", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var childSponsorshipNameAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "ChildSponsorshipName", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var leadershipDevelopmentAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "LeadershipDevelopment", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var paidChildcareWorkerAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "PaidChildcareWorker", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var hireDateAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "HireDate", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var w9Attribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "W9", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var childDedicationDateAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "ChildDedicationDate", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var photoConsentAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "PhotoConsent", StringComparison.InvariantCultureIgnoreCase ) ) );
+            var baptismDateAttribute = AttributeCache.Read( personAttributes.FirstOrDefault( a => a.Key.Equals( "BaptismDate", StringComparison.InvariantCultureIgnoreCase ) ) );
+
 
             var existingPersonAttributeValues = new AttributeValueService( lookupContext ).Queryable().Where( a => personAttributes.Select( pa => pa.Id ).Any( pa => pa == a.AttributeId ) ).ToList();
             var newPeopleAttributes = new Dictionary<int, Person>();
@@ -79,53 +87,93 @@ namespace Excavator.F1
 
                                     //Attribute case handling here
 
-                                    if ( attributeName.Contains( "Child Sponsorship" ) && !person.Attributes.ContainsKey( childSponsorshipLocationAttribute.Key ) )
+                                    //Child Sponsorship
+                                    if ( attributeName.Contains( "Child Sponsorship" ) )
                                     {
-                                        switch ( attributeName )
+                                        //Child Sponsorship Location
+                                        if ( !person.Attributes.ContainsKey( childSponsorshipLocationAttribute.Key ) )
                                         {
-                                            case "Child Sponsorship-El Salvador":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "El Salvador" );
-                                                break;
-                                            case "Child Sponsorship-India":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "India" );
-                                                break;
+                                            switch ( attributeName )
+                                            {
+                                                case "Child Sponsorship-El Salvador":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "El Salvador" );
+                                                    break;
+                                                case "Child Sponsorship-India":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "India" );
+                                                    break;
 
-                                            case "Child Sponsorship-India ServLife":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "India ServLife" );
-                                                break;
+                                                case "Child Sponsorship-India ServLife":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "India ServLife" );
+                                                    break;
 
-                                            case "Child Sponsorship-Mexico":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "Mexico" );
-                                                break;
+                                                case "Child Sponsorship-Mexico":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "Mexico" );
+                                                    break;
 
-                                            case "Child Sponsorship-Mexico (El Nino)":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "Mexico (El Nino)" );
-                                                break;
+                                                case "Child Sponsorship-Mexico (El Nino)":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "Mexico (El Nino)" );
+                                                    break;
 
-                                            case "Child Sponsorship-Uganda":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "Uganda" );
-                                                break;
+                                                case "Child Sponsorship-Uganda":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "Uganda" );
+                                                    break;
 
-                                            case "Child Sponsorship-Uganda-CSP":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "Uganda-CSP" );
-                                                break;
+                                                case "Child Sponsorship-Uganda-CSP":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "Uganda-CSP" );
+                                                    break;
 
-                                            case "Child Sponsorship-Uganda-Mosaic":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "Uganda-Mosaic" );
-                                                break;
+                                                case "Child Sponsorship-Uganda-Mosaic":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "Uganda-Mosaic" );
+                                                    break;
 
-                                            case "Child Sponsorship-Vietnam":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "Vietnam" );
-                                                break;
+                                                case "Child Sponsorship-Vietnam":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "Vietnam" );
+                                                    break;
 
-                                            case "Child Sponsorship-Vietnam-Father's House":
-                                                AddPersonAttribute( childSponsorshipLocationAttribute, person, "Vietnam-Father's House" );
-                                                break;
+                                                case "Child Sponsorship-Vietnam-Father's House":
+                                                    AddPersonAttribute( childSponsorshipLocationAttribute, person, "Vietnam-Father's House" );
+                                                    break;
+                                            }
+                                        }
+
+                                        //Child Sponsorship Name
+                                        if ( !person.Attributes.ContainsKey( childSponsorshipNameAttribute.Key ) && !string.IsNullOrWhiteSpace(comment) )
+                                        {
+                                            AddPersonAttribute( childSponsorshipNameAttribute, person, comment );
+                                        }
+
+                                        if ( !person.Attributes.ContainsKey( childSponsorshipStartDateAttribute.Key ) && startDate.HasValue )
+                                        {
+                                            AddPersonAttribute( childSponsorshipStartDateAttribute, person, startDate.Value.ToString( "yyyy-MM-dd") );
                                         }
                                     }
-                                    else if ( attributeName.Contains( "Facebook" ) && !person.Attributes.ContainsKey( facebookAttribute.Key ) )
+                                    else if ( attributeName.Contains( "Leadership Development" ) && !person.Attributes.ContainsKey( leadershipDevelopmentAttribute.Key ) )
                                     {
-                                       //
+                                        var matchingStaffPerson = GetPersonKeys( individualId, null, includeVisitors: false );
+                                        if (matchingStaffPerson != null )
+                                        {
+                                            AddPersonAttribute( leadershipDevelopmentAttribute, person, matchingStaffPerson.PersonId.ToString() );
+                                        }
+                                    }
+                                    else if (attributeName.Contains( "Paid Childcare Worker" ) && !person.Attributes.ContainsKey( paidChildcareWorkerAttribute.Key ) )
+                                    {
+                                        AddPersonAttribute( paidChildcareWorkerAttribute, person, "True" );
+                                    }
+                                    else if ( attributeName.Contains( "Photo Consent" ) && !person.Attributes.ContainsKey( photoConsentAttribute.Key ) )
+                                    {
+                                        AddPersonAttribute( photoConsentAttribute, person, "True" );
+                                    }
+                                    else if (attributeName.Contains( "W-9 on file (W-9)" ) && !person.Attributes.ContainsKey( w9Attribute.Key ) )
+                                    {
+                                        AddPersonAttribute( w9Attribute, person, "True" );
+                                    }
+                                    else if (attributeName.Contains( "Baptismal Date") && !person.Attributes.ContainsKey( baptismDateAttribute.Key ) && startDate.HasValue )
+                                    {
+                                        AddPersonAttribute( baptismDateAttribute, person, startDate.Value.ToString( "yyyy-MM-dd" ));
+                                    }
+                                    else if ( attributeName.Contains( "Child Dedication Dat" ) && !person.Attributes.ContainsKey( childDedicationDateAttribute.Key ) )
+                                    {
+                                        AddPersonAttribute( childDedicationDateAttribute, person, startDate.Value.ToString( "yyyy-MM-dd" ) );
                                     }
 
                                     if ( !newPeopleAttributes.ContainsKey( matchingPerson.PersonId ) )
